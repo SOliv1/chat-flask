17c0c57853b7052e557bd272c18117736e0e4c3e
Sara Oliver <s.oliver1@icloud.com>
2020-01-26 19:38:50 +0000

    upgraded flask version

--- run.py
+++ run.py
@@ -1,22 +1,26 @@
 import os
-from flask import Flask, redirect
+from datetime import datetime
+from flask import Flask, redirect, render_template
 
 app = Flask(__name__)
 messages = []
 
+
 def add_messages(username, message):
     """Add messages to the `messages` list"""
-    messages.append("{}: {}".format(username, message))
+    now = datetime.now().strftime("%H:%M:%S")
+    messages.append("({}) {}: {}".format(now, username, message))
 
 
 def get_all_messages():
     """Get all of the messages and separate them with a `br`"""
     return "<br>".join(messages)
 
+
 @app.route("/")
 def index():
     """Main page with instructions"""
-    return "To send a message use: /USERNAME/MESSAGE"
+    return render_template("index.html")
 
 
 @app.route("/<username>")
@@ -24,10 +28,12 @@ def user(username):
     """Display chat messages"""
     return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
 
+
 @app.route("/<username>/<message>")
 def send_message(username, message):
     """Create a new message and redirect back to the chat page"""
     add_messages(username, message)
     return redirect("/" + username)
+
 
 app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)
