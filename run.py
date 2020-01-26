ee04cce2787bcb24476d6702d99574a0dbde9376
Sara Oliver <s.oliver1@icloud.com>
2020-01-26 20:28:03 +0000

    added cookies features to project

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 import os
 from datetime import datetime
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request, session
+
 
 app = Flask(__name__)
+app.secret_key = "randomstring123"
 messages = []
 
 
@@ -17,9 +19,15 @@ def get_all_messages():
     return "<br>".join(messages)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
     """Main page with instructions"""
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+
+    if "username" in session:
+        return redirect(session["username"])
+
     return render_template("index.html")
 
 
